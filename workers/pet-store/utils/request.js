dcaa0b82feca1dc773fa24431d9aaab9b6273c8c
denis.grimaliuc <denis.grimaliuc@crunchyroll.com>
2024-07-18 11:31:58 +0300

    Added new worker

---
+++ workers/pet-store/utils/request.js
@@ -0,0 +1,51 @@
+import { buildResp } from '../../../lib/response';
+
+export function getQueryParams(url) {
+  // Create a URL object from the input URL string
+  const parsedUrl = new URL(url);
+
+  // Extract the search parameters using URLSearchParams
+  const params = new URLSearchParams(parsedUrl.search);
+
+  // Convert the search parameters to an object
+  const queryParams = {};
+  for (const [key, value] of params) {
+    queryParams[key.toLowerCase()] = value;
+  }
+
+  return queryParams;
+}
+
+export function filterByStatus(items, status) {
+  let statuses = status.split('&');
+  return items.filter(item =>
+    statuses.every(s => {
+      if (s.startsWith('!')) {
+        return item.status !== s.slice(1);
+      } else {
+        return item.status === s;
+      }
+    }),
+  );
+}
+
+export function validateBody(body, props) {
+  const errors = props.filter(prop => !body[prop]);
+
+  if (errors.length > 0) {
+    return buildResp({ body: { error: 'Props are required: ' + errors }, status: 400 });
+  }
+
+  return null;
+}
+
+export function extractUUID(url) {
+  // Simplified regular expression to match UUID format
+  const regex = /[0-9a-f]{8}-(?:[0-9a-f]{4}-){3}[0-9a-f]{12}/i;
+
+  // Extract UUID from the URL using regex
+  const match = url.match(regex);
+
+  // Return the extracted UUID or null if not found
+  return match ? match[0] : null;
+}
